d460cb8d91cfbdf60b1ce43f8885057c6714e5b3
Hardik Sharma <72403424+HARDIK-SHARMA-08@users.noreply.github.com>
2023-12-17 03:29:07 +0530

    Update Stories.tsx

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -36,11 +36,6 @@ export const Stories = () => {
       duration: 5000,
     },
     {
-      url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
-      type: "video",
-      preloadResource: false,
-    },
-    {
       url: "https://plus.unsplash.com/premium_photo-1676231417481-5eae894e7f68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80",
       preloadResource: false,
     },
