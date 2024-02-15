313edbaf1859d09391c84420ef33e6cb724f3b48
HARDIK-SHARMA-08 <hardiksharma602@gmail.com>
2024-02-16 00:25:43 +0530

    Changes

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -4,8 +4,8 @@ import { CalendarIcon, LinkIcon, LocationIcon } from "./icons";
 
 export const Experience = () => {
   return (
-    <div>
-      <div className="h-20 w-full rounded-2xl p-4 sm:p-10 bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-rose-400 via-fuchsia-500 to-indigo-500 dark:bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] dark:from-slate-900 dark:via-purple-900 dark:to-slate-900 flex justify-center items-center relative">
+    <div className="h-full">
+      <div className="h-20 w-full flex justify-center items-center rounded-2xl p-4 sm:p-10 bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-rose-400 via-fuchsia-500 to-indigo-500 dark:bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] dark:from-slate-900 dark:via-purple-900 dark:to-slate-900 ">
         <div>
           <Image
             src="work.png"
@@ -17,48 +17,64 @@ export const Experience = () => {
           Experience
         </h4>
       </div>
-      <div className="flex justify-center flex-col h-fit lg:h-full pt-4 pb-0 px-4 lg:pb-4  ">
-        <ul
-          role="list"
-          className="marker:text-neutral-700 marker:text-3xl marker:font-extrabold list-decimal pl-5 space-y-3"
-        >
-          <li className="flex flex-col">
-            <li>
-              <div className="uppercase font-bold text-md inline-flex items-center gap-2 lg:text-xl hover:underline">
-                Software Developer Intern <LinkIcon />
-              </div>
 
-              <div className="text-sm text-gray-500">
-                Skinthirst-Vyomaga Skincare Pvt. Ltd.{" "}
-              </div>
-              <div className="text-sm text-gray-500 inline-flex gap-2 items-center">
-                <CalendarIcon /> July 2023 - August 2023{" "}
-              </div>
-              <br />
-              <div className="text-sm text-gray-500 inline-flex gap-2 items-center">
-                <LocationIcon /> Mumbai, India{" "}
-              </div>
-            </li>
-          </li>
-          <li className="flex flex-col">
-            <li>
+      <div className="flex flex-col h-5/6 justify-evenly gap-4 px-1 sm:px-6 py-4">
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-row justify-around items-center">
+            <Image
+              src="vanshiv.png"
+              className="w-12 sm:w-12 md:w-20 lg:w-22"
+              alt="Picture of the author"
+            />
+            <div>
               <div className="uppercase font-bold text-md inline-flex items-center gap-2 lg:text-xl hover:underline">
-                Software Developer Intern<LinkIcon />
+                Software Developer <LinkIcon />
               </div>
               <div className="text-sm text-gray-500">
-                Airdrive Smart Solutions{" "}
+                Vanshiv Technologies - Intern
               </div>
-              <div className="text-sm text-gray-500 inline-flex gap-2 items-center">
-                <CalendarIcon /> May 2023 - June 2023{" "}
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row justify-between gap-2">
+            <div className="text-sm text-gray-500 inline-flex gap-2 items-center">
+              <CalendarIcon /> Febuary 2024 - Present{" "}
+            </div>
+            <div className="text-sm text-gray-500 inline-flex gap-2 items-center">
+              <LocationIcon /> Jaipur, India{" "}
+            </div>
+          </div>
+        </div>
+
+
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-row justify-around items-center">
+            <Image
+              src="skinthirst.png"
+              className="w-12 sm:w-12 md:w-20 lg:w-22"
+              alt="Picture of the author"
+            />
+
+            <div>
+              <div className="uppercase font-bold text-md inline-flex items-center gap-2 lg:text-xl hover:underline">
+                Software Developer<LinkIcon />
               </div>
-              <br />
-              <div className="text-sm text-gray-500 inline-flex gap-2 items-center">
-                <LocationIcon /> Hyderabad, India{" "}
+              <div className="text-sm text-gray-500">
+                Skinthirst skincare Pvt. Ltd. - Intern
               </div>
-            </li>
-          </li>
-        </ul>
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row justify-between gap-2">
+            <div className="text-sm text-gray-500 inline-flex gap-2 items-center">
+              <CalendarIcon /> July 2023 - August 2023{" "}
+            </div>
+            <div className="text-sm text-gray-500 inline-flex gap-2 items-center">
+              <LocationIcon /> Hyderabad, India{" "}
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
+
   );
 };
